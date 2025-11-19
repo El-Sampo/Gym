4ad28d3c7ca68df903bc55d7c4430e61cef3bba3
El-Sampo <mohamedhanikamal2004@gmail.com>
2025-11-19 14:59:46 +0200

    Revert "."
    This reverts commit 08498aa08383528566f834a11990a5dfe01cc979.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Home from '../components/Home.jsx'
+import Home from '../components/Home'
 const HomePage = () => {
   return (
     <div id = "home" className='mt-[70px]'>
