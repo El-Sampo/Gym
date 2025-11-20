26e288a7447631bafb066b2612b230369428e3a2
KarimHazem12 <karimhazem01@gmail.com>
2025-11-20 13:55:40 +0200

    update vite

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react() , tailwindcss()],
+  build: {
+    chunkSizeWarningLimit: 1000, // Adjust the limit in KiB as needed (e.g., 1000 for 1MB)
+  },
+
 })
